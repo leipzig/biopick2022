a1e25ed9a4ee556443a926109447d2e8d908688c
Jeremy Leipzig <leipzig@gmail.com>
2022-01-27 14:23:50 -0700

    color

--- biopick.R
+++ biopick.R
@@ -41,10 +41,15 @@ results %>% arrange(-nrml_price) %>% knitr::kable() -> mktable
 
 
 myplot<-ggplot(results, aes(freq, nrml_price)) +
-  geom_point() +
+  geom_point(aes(colour=nrml_price)) +
+  scale_colour_gradient2(
+    low = "red",
+    high = "green",
+    midpoint = 100
+  ) +
   geom_text(
     data = subset(results, freq > 10 |
-                    nrml_price > 300),
+                    nrml_price > 110),
     aes(freq, nrml_price, label = paste0(ticker)),
     check_overlap = FALSE,
     position = position_jitter(width = 2, height = 2),
@@ -57,7 +62,7 @@ myplot<-ggplot(results, aes(freq, nrml_price)) +
     position = position_jitter(width = 2, height = 2),
     vjust = 1.2
   ) +
-  scale_y_continuous(breaks = seq(0, 1000, 100)) + 
+  scale_y_continuous(breaks = seq(0, 1000, 20)) + 
   xlim(-5, 70)+xlab("Number of Picks")+ylab(paste(Sys.Date(),"percent of price on 01/03/2022"))
 ggsave(filename = "biopicks.png",plot = myplot)
 
